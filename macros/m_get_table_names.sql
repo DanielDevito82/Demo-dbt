689905a53b4bf3cf235a717eff3ab293497ad938
Daniel Mestrovic <daniel.mestrovic@sulzer.de>
2024-09-15 15:53:15 +0000

    Final Union from Bronze Reddit API Layer to Silver Layer

--- macros/m_get_table_names.sql
+++ macros/m_get_table_names.sql
@@ -1,10 +1,23 @@
-{% macro m_get_table_names(schema_name) %}
+{% macro m_get_table_names(schema_name, filter_names) %}
     {% set query %}
         SELECT table_name
         FROM portfolio_analyse.information_schema.tables
-        WHERE table_schema = '{{ schema_name }}'
+        WHERE table_schema = '{{ schema_name }}' AND table_name LIKE '%{{filter_names}}'
     {% endset %}
+    
     {% set results = run_query(query) %}
-    {% set table_names = results.columns['table_name'] %}
+    
+    {% if results is none %}
+        {{ exceptions.raise_compiler_error("No results returned for query: " ~ query) }}
+    {% endif %}
+    
+    {{ log("Results: " ~ results, info=True) }}
+    
+    {% set table_names = [] %}
+    {% for row in results %}
+        {% do table_names.append(row['table_name']) %}
+    {% endfor %}
+    
+    {{ log("Table names: " ~ table_names, info=True) }}
     {{ return(table_names) }}
 {% endmacro %}
